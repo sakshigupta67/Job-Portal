17a278029312cc0f663d9b754c1481d9f5c784d8
Sakshi Gupta <sakshimailara@gmail.com>
2025-09-23 22:04:07 +0530

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import{ Route , Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import ApplyJob from './pages/ApplyJob'
+import Applications from './pages/Applications'
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      
+      <Routes>
+
+        <Route path='/' element= {<Home />}/>
+     
+         <Route path='/' element= {<Home />}/>
+         <Route path='/apply-job/:id' element= {<ApplyJob />}/>
+<Route path='/applications' element= {<Applications />}/>
+ 
+       </Routes>
+      
+      </div>
+
+
   )
 }
 
