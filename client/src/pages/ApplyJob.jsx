a0259c8dcb93c06959a10f6deab8cecbc135f516
Sakshi Gupta <sakshimailara@gmail.com>
2025-10-16 17:33:48 +0530

    Update ApplyJob.jsx

--- client/src/pages/ApplyJob.jsx
+++ client/src/pages/ApplyJob.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
-import {useParams} from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 import { assets, jobsData } from '../assets/assets'
 import Loading from '../components/Loading'
@@ -11,89 +11,86 @@ import moment from 'moment'
 
 const ApplyJob = () => {
 
-  
+
   const { id } = useParams()
 
   const [JobData, setJobData] = useState(null)
 
-  const {jobs} = useContext(AppContext)
-  
-  const fetchJob = async()=> {
+  const { jobs } = useContext(AppContext)
+
+  const fetchJob = async () => {
     const data = jobs.filter(job => job._id === id)
-if(data.length !== 0){
-  setJobData(data[0])
-  console.log(data[0])
-} 
- }
-  
- useEffect( () => {
-if(jobs.length > 0){
-  fetchJob()
-}
- }, [id, jobs])
+    if (data.length !== 0) {
+      setJobData(data[0])
+      console.log(data[0])
+    }
+  }
+
+  useEffect(() => {
+    if (jobs.length > 0) {
+      fetchJob()
+    }
+  }, [id, jobs])
 
 
   return JobData ? (
     <>
-<Navbar />
+      <Navbar />
 
-<div className='min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto'>
-  <div className='bg-white text-black rounded-lg w-full '>
-<div className='flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl'>
-  <div className='flex flex-col md:flex-row items-center'>
-
-
-<img className ='h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border' src={JobData.companyId.image} alt="" />
-<div>
-  <h1>{JobData.title}</h1>
-
-  <div>
-<span>
-  <img src={assets.suitcase_icon} alt="" />
-  {JobData.companyId.name}
-</span>
+      <div className='min-h-screen flex flex-col py-10 container px-4 2xl:px-20 mx-auto'>
+        <div className='bg-white text-black rounded-lg w-full '>
+          <div className='flex justify-center md:justify-between flex-wrap gap-8 px-14 py-20 mb-6 bg-sky-50 border border-sky-400 rounded-xl'>
+            <div className='flex flex-col md:flex-row items-center'>
 
 
-<span>
-<img src={assets.location_icon} alt="" />
-{JobData.location}
+              <img className='h-24 bg-white rounded-lg p-4 mr-4 max-md:mb-4 border' src={JobData.companyId.image} alt="" />
+              <div className='text-center md:text-left text-neutral-700'>
+                <h1 className='text-2xl sm:text-4xl font-medium'>{JobData.title}</h1>
 
-</span>
-
-<span>
-<img src={assets.person_icon} alt="" />
-{JobData.level}
-
-</span>
-<span>
-
-<img src={assets.money_icon} alt="" />
-CTC:{kconvert.convertTo(JobData.salary)}
-</span>
+                <div className='flex flex-row flex-wrap max-md:justify-center gap-y-2 gap-6 items-center text-gray-600'>
+                  <span className='flex items-center gap-1'>
+                    <img src={assets.suitcase_icon} alt="" />
+                    {JobData.companyId.name}
+                  </span>
 
 
-  </div>
-</div>
-  </div>
+                  <span className='flex items-center gap-1'>
+                    <img src={assets.location_icon} alt="" />
+                    {JobData.location}
 
-<div>
-  <button>Apply Now</button>
-  <p>Posted {moment(JobData.date).fromNow()}</p>
-</div>
+                  </span>
+
+                  <span className='flex items-center gap-1'>
+                    <img src={assets.person_icon} alt="" />
+                    {JobData.level}
+
+                  </span>
+                  <span >
+
+                    <img src={assets.money_icon} alt="" />
+                    CTC:{kconvert.convertTo(JobData.salary)}
+                  </span>
 
 
-</div>
-  </div>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <button>Apply Now</button>
+              <p>Posted {moment(JobData.date).fromNow()}</p>
+            </div>
 
 
+          </div>
+        </div>
 
-
-</div>
+      </div>
 
 
     </>
   ) : (
-  <Loading />
+    <Loading />
   )
 }
 
