0e77d1d236e54d8e790774d75ad56e1fae42bd0c
Sakshi Gupta <sakshimailara@gmail.com>
2025-12-27 01:08:01 +0530

    added vercel.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ import { connect } from 'mongoose';
 import connectDB from './config/db.js';
 
 import * as Sentry from "@sentry/node";
+import { clerkWebhooks } from './controllers/webhooks.js';
 
 
 
@@ -25,6 +26,7 @@ app.get('/' , (req, res) =>res.send("API Working"))
 app.get("/debug-sentry", function mainHandler(req, res) {
   throw new Error("My first Sentry error!");
 });
+app.post('/webhooks' , clerkWebhooks)
 
 //PORT
 const PORT = process.env.PORT || 5000
